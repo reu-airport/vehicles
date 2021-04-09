cc9d96d90815362aee6601979561091669cdd156
Denis Yakovlev <denn.yakovlev@gmail.com>
2021-04-09 20:17:10 +0300

    Action contexts added

---
+++ AirportVehicles/ActionsContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportVehicles
+{
+    abstract class ActionsContext
+    {
+        protected LinkedList<VehicleAction> actions;
+        protected LinkedListNode<VehicleAction> currentActionNode;
+        protected VehicleAction CurrentAction => currentActionNode.Value;
+
+        protected Vehicle Vehicle { get; }
+
+        public bool IsRunning { get; private set; }
+
+        public event EventHandler Completed;
+
+        public ActionsContext(Vehicle vehicle, IEnumerable<VehicleAction> actions)
+        {
+            Vehicle = vehicle;
+            this.actions = new LinkedList<VehicleAction>(actions);
+            foreach (var action in this.actions)
+            {
+                action.Vehicle = Vehicle;
+                action.Done += MoveToNextAction;
+            }
+            Completed += (s, e) => { IsRunning = false; };
+        }
+
+        public abstract void Run();
+
+        protected void OnCompleted(object? sender, EventArgs e) =>
+            Completed?.Invoke(sender, e);
+
+        protected void MoveToNextAction(object? sender, EventArgs e)
+        {
+            currentActionNode = currentActionNode.Next;
+            CurrentAction.Initiate();
+        }
+    }
+}
---
+++ AirportVehicles/PlainActionsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportVehicles
+{
+    class PlainActionsContext : ActionsContext
+    {
+        public PlainActionsContext(Vehicle vehicle, params VehicleAction[] actions):
+            base(vehicle, actions)
+        {
+            this.actions.Last.Value.Done += (s, e) => OnCompleted(this, EventArgs.Empty);
+        }
+
+        public override void Run()
+        {
+            currentActionNode = actions.First;
+            CurrentAction.Initiate();
+        }
+    }
+}
